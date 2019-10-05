c3df72d14366d3136e79d0256d94ab35821d5c18
Renan <renan_ota@hotmail.com>
2019-10-05 01:59:39 -0300

    Identificados anuncios novos

--- checker.py
+++ checker.py
@@ -8,19 +8,18 @@ solicitacoes = get_solicitacoes()
 for i in solicitacoes:
     produto = i.produto
     anuncios_salvos = get_anuncios_salvos(i.id)
-    teste = [i.titulo for i in anuncios_salvos]
-    print(teste)
+    titulos_produtos_salvos = [i.titulo for i in anuncios_salvos]
 
-    anuncios = web_scrap(produto)
-    teste2 = [i['titulo'] for i in anuncios]
-    print(teste2)
+    anuncios_encontrados = web_scrap(produto)
+    titulos_anuncios_encontrados = [i['titulo'] for i in anuncios_encontrados]
+
+    novos_anuncios = [x for x in titulos_anuncios_encontrados if x not in anuncios_salvos]
+    
 
 
 
 # Fazer o web scrap
-
 # Verificar resulados com os já existentes
-
 # Caso haja diferença, enviar a mensagem pelo bot
 
 # Fazer o web_scrap
