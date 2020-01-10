1e90277fcec9f35186f28c64ba7a45e78213d9a6
DocMcCoy <renan_ota@hotmail.com>
2020-01-10 00:36:12 -0300

    Adicionado tratamento para que seja possível escolher entre sqlite e mysql no config.ini

--- db_connection.py
+++ db_connection.py
@@ -1,12 +1,15 @@
-from peewee import MySQLDatabase #, SqliteDatabase
+from peewee import MySQLDatabase, SqliteDatabase
 from params import db_params
 
 
-# db = SqliteDatabase('database.db')
-db = MySQLDatabase(
-    db_params['database'],
-    host = db_params['host'],
-    port = int(db_params['port']),
-    user = db_params['user'],
-    password = db_params['password']
-)
+if db_params['tipe'] == 'sqlite':
+	db = SqliteDatabase('database.db')
+
+elif db_params['tipe'] == 'mysql':
+	db = MySQLDatabase(
+		db_params['database'],
+		host = db_params['host'],
+		port = int(db_params['port']),
+		user = db_params['user'],
+		password = db_params['password']
+	)
