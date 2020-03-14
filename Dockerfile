da8736cb242a0bdf3c5c6bdf4b2a0bceb6b3d362
Renan Santana <renan_ota@hotmail.com>
2020-03-14 17:42:04 -0300

    Database alterado para postgres

--- Dockerfile
+++ Dockerfile
@@ -6,6 +6,5 @@ COPY requirements.txt ./
 RUN pip install --no-cache-dir -r requirements.txt
 
 COPY ./src .
-COPY config.ini .
 
-CMD [ "python", "listener.py"]
+CMD [ "python", "listener.py" ]
--- src/database/connection.py
+++ src/database/connection.py
@@ -1,15 +1,11 @@
-from peewee import MySQLDatabase, SqliteDatabase
+from peewee import PostgresqlDatabase
 from params import db_params
 
 
-if db_params['type'] == 'sqlite':
-	db = SqliteDatabase('database.db')
-
-elif db_params['type'] == 'mysql':
-	db = MySQLDatabase(
-		db_params['database'],
-		host = db_params['host'],
-		port = int(db_params['port']),
-		user = db_params['user'],
-		password = db_params['password']
-	)
+db = PostgresqlDatabase(
+	db_params['database'],
+	host = db_params['host'],
+	port = int(db_params['port']),
+	user = db_params['user'],
+	password = db_params['password']
+)
